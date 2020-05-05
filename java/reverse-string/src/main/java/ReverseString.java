a7bf8c00c0343ba4eea9bf49619ca5a26c047f5a
Nikita <nkriuckov@gmail.com>
2020-05-05 21:43:43 +0300

    first commit
    first commit

---
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -0,0 +1,9 @@
+
+class ReverseString {
+
+    String reverse(String inputString) {
+        StringBuilder rev = new StringBuilder(inputString).reverse();
+        return rev.toString();
+    }
+  
+}
