fc3f051a27456dc8b488dd0d0f68c4490602e0e1
Nikita <nkriuckov@gmail.com>
2020-05-07 18:55:08 +0300

    first commit

--- java/resistor-color-duo/src/main/java/ResistorColorDuo.java
+++ java/resistor-color-duo/src/main/java/ResistorColorDuo.java
@@ -1,5 +1,38 @@
+import java.util.HashMap;
+
 class ResistorColorDuo {
     int value(String[] colors) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+           
+            String[] resCols = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};     
+            int num1 = 0;
+            int num2 = 0;
+
+            for (int i = 0; i < 1; i++) {
+                for (int j = 0; j < resCols.length; j++) {
+                    if (colors[0] == resCols[j]) {
+                        num1 = j;
+                    }
+                    if (colors[1] == resCols[j]) {
+                        num2 = j;
+                    }
+                }
+            }
+            String one = String.valueOf(num1);
+            String two = String.valueOf(num2);
+            String result = one.concat(two);
+
+
+        
+
+            return Integer.parseInt(result);
+
+          
+            
+
+
+
+            
+
+        
     }
 }
