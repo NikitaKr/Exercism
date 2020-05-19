1d96dae893e7102b71eee08f089becd41c36c426
Nikita <nkriuckov@gmail.com>
2020-05-19 19:36:44 +0300

    all tests green

--- java/acronym/src/main/java/Acronym.java
+++ java/acronym/src/main/java/Acronym.java
@@ -6,11 +6,22 @@ class Acronym {
     
     Acronym(String phrase) {
         for (int i = 0; i < phrase.length(); i++) {
-            if(Character.isUpperCase(phrase.charAt(i)) || Character.isWhitespace(phrase.charAt(i)) ) {
-                acronym = acronym.concat(String.valueOf(phrase.charAt(i+1)));
+            if (i == 0) {
+                if(Character.isUpperCase(phrase.charAt(i))) {
+                    acronym = acronym.concat(String.valueOf(phrase.charAt(i)));
+            } }
+            if (i > 0) {
+                if (Character.isWhitespace(phrase.charAt(i-1)) || phrase.charAt(i-1) == '-' || phrase.charAt(i-1) == '_') {
+                    if(phrase.charAt(i) != ' ' && phrase.charAt(i) != '-' && phrase.charAt(i) != '_') {
+                    acronym = acronym.concat(String.valueOf(phrase.charAt(i))); }
+                }
             }
+        
+            }
+
+        acronym = acronym.toUpperCase();
         }
-    }
+    
 
     String get() {
        return acronym;
