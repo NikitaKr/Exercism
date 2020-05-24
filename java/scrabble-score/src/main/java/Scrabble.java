c63eb3c3a96f5697bb1df6e5ad0ab1f12a239071
Nikita <nkriuckov@gmail.com>
2020-05-24 20:42:35 +0300

    fc

---
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -0,0 +1,20 @@
+class Scrabble {
+
+    private int score;
+
+    Scrabble(String word) {
+
+           String[] one = new String[] {"A", "E", "I", "O", "U", "L", "N", "R", "S", "T"}; 
+
+
+        for (int i = 0; i < word.length(); i++) {
+            if ()
+            
+        }
+    }
+
+    int getScore() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
