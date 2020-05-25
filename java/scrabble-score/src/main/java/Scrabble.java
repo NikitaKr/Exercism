5bc853e647295fe6774950e76b6c7a7845a28c64
Nikita <nkriuckov@gmail.com>
2020-05-25 20:50:38 +0300

    Scrabble all tests green

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,20 +1,40 @@
 class Scrabble {
 
-    private int score;
+    private int score = 0;
 
     Scrabble(String word) {
 
-           String[] one = new String[] {"A", "E", "I", "O", "U", "L", "N", "R", "S", "T"}; 
-
+           word = word.toUpperCase();
+          
+           String one = "AEIOULNRST";
+           String two = "DG";
+           String three = "BCMP";
+           String four = "FHVWY";
+           String five = "K";
+           String six = "JX";
+           String seven = "QZ";
 
         for (int i = 0; i < word.length(); i++) {
-            if ()
-            
+           if(one.contains(String.valueOf(word.charAt(i)))) {
+               score = score + 1;
+           } else if(two.contains(String.valueOf(word.charAt(i)))) {
+               score = score + 2;
+           } else if(three.contains(String.valueOf(word.charAt(i)))) {
+            score = score + 3;
+           } else if(four.contains(String.valueOf(word.charAt(i)))) {
+            score = score + 4;
+           } else if(five.contains(String.valueOf(word.charAt(i)))) {
+            score = score + 5;
+           } else if(six.contains(String.valueOf(word.charAt(i)))) {
+            score = score + 8;
+           } else if(seven.contains(String.valueOf(word.charAt(i)))) {
+            score = score + 10;
+           }
         }
     }
 
     int getScore() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return score;
     }
 
 }
