d95fdae3494eab8ac62537c956ace1d05ee450c8
Nikita <nkriuckov@gmail.com>
2020-05-26 16:52:31 +0300

    Difference of square - all tests green

--- java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,15 +1,27 @@
+import java.lang.*;
+
 class DifferenceOfSquaresCalculator {
 
+    int squareOfSums = 0;
+    int sumOfSquares = 0;
+
     int computeSquareOfSumTo(int input) {
-        
+        for (int i = 0; i < input + 1; i++) {
+            squareOfSums += i;
+        }
+        return (squareOfSums*squareOfSums);
     }
 
     int computeSumOfSquaresTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        for (int i = 0; i < input + 1; i++) {
+            sumOfSquares += (i*i);
+        }
+        return sumOfSquares;
     }
 
     int computeDifferenceOfSquares(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int diff = computeSquareOfSumTo(input) - computeSumOfSquaresTo(input);
+        return diff;
     }
 
 }
