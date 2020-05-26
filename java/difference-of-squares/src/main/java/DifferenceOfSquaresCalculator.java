38ee9746be39dee3db193b66fdb38344bccc3677
Nikita <nkriuckov@gmail.com>
2020-05-26 13:59:59 +0300

    DifferenceOfSquaresCalculator
    DifferenceOfSquaresCalculator first commit

--- java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,7 +1,7 @@
 class DifferenceOfSquaresCalculator {
 
     int computeSquareOfSumTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        
     }
 
     int computeSumOfSquaresTo(int input) {
