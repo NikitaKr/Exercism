01ef1a39a7ba33f0c98a7db9c5af3554fb51f3d4
Nikita <nkriuckov@gmail.com>
2020-05-26 13:35:11 +0300

    RaindropConverter
    RaindropConverter all tests green

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,41 @@
 class RaindropConverter {
 
-    String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    private String phrase = ""; 
+
+    String convert(final int number) {
+        if (number % 3 == 0) {
+            if (phrase.isEmpty()) {
+                phrase = "Pling"; 
+            } else {
+                phrase += "Pling";
+            }
+        } 
+
+        if (number % 5 == 0) {
+            if (phrase.isEmpty()) {
+                phrase = "Plang"; 
+            } else {
+                phrase += "Plang";
+            }
+        } 
+
+        if (number % 7 == 0) {
+            if (phrase.isEmpty()) {
+                phrase = "Plong"; 
+            } else {
+                phrase += "Plong";
+            }
+        } 
+
+        if(phrase.isEmpty()) {
+            phrase = String.valueOf(number);
+        }
+        
+
+        return phrase;
+    
     }
 
+    
 }
+
